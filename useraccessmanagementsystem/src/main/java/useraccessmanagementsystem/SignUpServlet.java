5747c00ecab9c7c79a5ae10f3c06e8dfcfb48a55
ahteshama01 <168338537+ahteshama01@users.noreply.github.com>
2024-11-14 13:20:24 +0530

    Update SignUpServlet.java

--- useraccessmanagementsystem/src/main/java/useraccessmanagementsystem/SignUpServlet.java
+++ useraccessmanagementsystem/src/main/java/useraccessmanagementsystem/SignUpServlet.java
@@ -1,10 +1,12 @@
 package useraccessmanagementsystem;
-
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+
+@WebServlet("/signup")
 public class SignUpServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
