4e2a1dfdb5cb02b8fd187c3b8984d92a1d6da400
ahteshama01 <168338537+ahteshama01@users.noreply.github.com>
2024-11-14 13:18:20 +0530

    Update LoginServlet.java

--- useraccessmanagementsystem/src/main/java/useraccessmanagementsystem/LoginServlet.java
+++ useraccessmanagementsystem/src/main/java/useraccessmanagementsystem/LoginServlet.java
@@ -1,10 +1,11 @@
 package useraccessmanagementsystem;
-
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.sql.*;
 
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
